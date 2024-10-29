4e57e2e721e1d83a87132e07ed77d8d9adb5f772
TLOWAC <leechanghun55@gmail.com>
2024-10-29 10:40:30 +0900

    feat(api): user | 삭제 기능 추가

--- go-basic-server/api/server.go
+++ go-basic-server/api/server.go
@@ -3,6 +3,7 @@ package api
 import (
 	"encoding/json"
 	"module/storage"
+	"module/util"
 	"net/http"
 )
 
@@ -20,11 +21,20 @@ func NewServer(listenAddr string, store storage.Storage) *Server {
 
 func (s *Server) Start() error {
 	http.HandleFunc("/user", s.handleGetUserById)
+	http.HandleFunc("/user/id", s.handleDeleteUserById)
 	return http.ListenAndServe(s.listenAddr, nil)
 }
 
 func (s *Server) handleGetUserById(w http.ResponseWriter, r *http.Request) {
 	user := s.store.Get(10)
+
+	json.NewEncoder(w).Encode(user)
+}
+
+func (s *Server) handleDeleteUserById(w http.ResponseWriter, r *http.Request) {
+	user := s.store.Get(10)
+
+	_ = util.Round2Dec(10.34434)
 
 	json.NewEncoder(w).Encode(user)
 }

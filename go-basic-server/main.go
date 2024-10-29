3154966e3542887f3aed107add5ff8d3cf082cd8
TLOWAC <leechanghun55@gmail.com>
2024-10-29 10:31:58 +0900

    feat: storage 의존성 주입

--- go-basic-server/main.go
+++ go-basic-server/main.go
@@ -5,13 +5,16 @@ import (
 	"fmt"
 	"log"
 	"module/api"
+	"module/storage"
 )
 
 func main() {
 	listenAddr := flag.String("listenaddr", ":3000", "the server address")
 	flag.Parse()
 
-	server := api.NewServer(*listenAddr)
+	store := storage.NewMemoryStorage()
+
+	server := api.NewServer(*listenAddr, store)
 	fmt.Println("server running on port:", *listenAddr)
 	log.Fatal(server.Start())
 

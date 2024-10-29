f2b1099d7807c12ca994f0a39ddd7fcd15317be9
TLOWAC <leechanghun55@gmail.com>
2024-10-29 10:14:26 +0900

    feat: addr 플래그 기능 추가 및 서버 구동 코드 제작

--- go-basic-server/main.go
+++ go-basic-server/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main(){
-	
+import (
+	"flag"
+	"fmt"
+	"log"
+	"module/api"
+)
+
+func main() {
+	listenAddr := flag.String("listenaddr", ":3000", "the server address")
+	flag.Parse()
+
+	server := api.NewServer(*listenAddr)
+	fmt.Println("server running on port:", *listenAddr)
+	log.Fatal(server.Start())
+
 }

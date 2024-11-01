d2e116e620ddb7f423371eee3f059f682a4924de
TLOWAC <leechanghun55@gmail.com>
2024-11-01 13:41:50 +0900

    refactor: 페이지 구성

--- go-react-fullstack/client/src/App.tsx
+++ go-react-fullstack/client/src/App.tsx
@@ -1,17 +1,20 @@
 import { Container, Stack } from "@chakra-ui/react";
 import Navbar from "./components/ui/Navbar";
+import TodoForm from "./components/ui/TodoForm";
+import TodoList from "./components/ui/TodoList";
+
+export const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
 
 function App() {
-
-  return (
-    <Stack h="100vh">
-      <Navbar/>
-      <Container>
-        {/* <TodoForm/>
-        <TodoList/> */}
-      </Container>
-    </Stack>
-  )
+	return (
+		<Stack h='100vh'>
+			<Navbar />
+			<Container>
+				<TodoForm />
+				<TodoList />
+			</Container>
+		</Stack>
+	);
 }
 
-export default App
+export default App;

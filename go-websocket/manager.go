57d511d8ceaafafd8a3f3ed63a3f67f66f5d08de
TLOWAC <leechanghun55@gmail.com>
2024-11-05 23:01:25 +0900

    feat: routeEvent 추가

--- go-websocket/manager.go
+++ go-websocket/manager.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"log"
 	"net/http"
 	"sync"
@@ -18,11 +20,45 @@ var (
 type Manager struct {
 	clients ClientList
 	sync.RWMutex
+
+	// switch-case 로 분기 처리를 하는 경우 depth 가 깊고 길이가 길어지므로 key-value 식으로 key 에 event 를 매칭 시켜 사용하는 방식
+	handlers map[string]EventHandler
 }
 
 func NewManager() *Manager {
-	return &Manager{
-		clients: make(ClientList),
+	m := &Manager{
+		clients:  make(ClientList),
+		handlers: make(map[string]EventHandler),
+	}
+
+	m.setupEventHandlers()
+
+	return m
+}
+
+func (m *Manager) setupEventHandlers() {
+	// 'EventSendMessage' 이벤트가 들어오면, 맵핑되어 있는 'SendMessage' 실행하기 위해
+	// 이벤트-실행 함수를 맵핑
+	m.handlers[EventSendMessage] = SendMessage
+}
+
+func SendMessage(event Event, c *Client) error {
+	fmt.Print(event)
+	return nil
+}
+
+// 이벤트가 들어오는 경우.
+// 예외처리 및 라우팅
+func (m *Manager) routeEvent(event Event, c *Client) error {
+	// 핸들러에 해당 이벤트 타입이 정의되어 있는지 체크
+	if handler, ok := m.handlers[event.Type]; ok {
+		// 해당 이벤트 타입이 정의되어 있는 경우 handler 에서 해당 이벤트를 실행
+		if err := handler(event, c); err != nil {
+			return err
+		}
+		return nil
+	} else {
+		return errors.New("there is no such event type")
 	}
 }
 

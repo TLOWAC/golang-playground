a546f2c82256f0db7f0eba43964ac8aab83f0927
TLOWAC <leechanghun55@gmail.com>
2024-11-05 20:49:13 +0900

    feat: serveWs 구성

---
+++ go-websocket/manager.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"sync"
+
+	"github.com/gorilla/websocket"
+)
+
+var (
+	websocketUpgrader = websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+	}
+)
+
+type Manager struct {
+	clients ClientList
+	sync.RWMutex
+}
+
+func NewManager() *Manager {
+	return &Manager{
+		clients: make(ClientList),
+	}
+}
+
+func (m *Manager) serveWS(w http.ResponseWriter, r *http.Request) {
+	log.Println("New Connection")
+
+	// upgrade regular http connection into websocket connection.
+	// This function is called whenever websocket upgrade connection request is requested
+	conn, err := websocketUpgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	client := NewClient(conn, m)
+	m.addClient(client)
+
+	conn.Close()
+}

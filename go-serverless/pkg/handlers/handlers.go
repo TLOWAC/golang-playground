968e4cf55fae367d16261ad97d92c5fc77bfc969
TLOWAC <leechanghun55@gmail.com>
2024-11-11 02:38:10 +0900

    feat(handler): create, update, delete 기능 추가

--- go-serverless/pkg/handlers/handlers.go
+++ go-serverless/pkg/handlers/handlers.go
@@ -33,13 +33,31 @@ func GetUser(req events.APIGatewayProxyRequest, tableName string, db dynamodbifa
 	return apiResponse(http.StatusOK, result)
 }
 func CreateUser(req events.APIGatewayProxyRequest, tableName string, db dynamodbiface.DynamoDBAPI) (*events.APIGatewayProxyResponse, error) {
-	return nil
+	result, err := user.CreateUser(req, tableName, db)
+	if err != nil {
+		return apiResponse(http.StatusBadRequest, ErrorBody{
+			aws.String(err.Error()),
+		})
+	}
+	return apiResponse(http.StatusOK, result)
 }
 func UpdateUser(req events.APIGatewayProxyRequest, tableName string, db dynamodbiface.DynamoDBAPI) (*events.APIGatewayProxyResponse, error) {
-	return nil
+	result, err := user.UpdateUser(req, tableName, db)
+	if err != nil {
+		return apiResponse(http.StatusBadRequest, ErrorBody{
+			aws.String(err.Error()),
+		})
+	}
+	return apiResponse(http.StatusOK, result)
 }
 func DeleteUser(req events.APIGatewayProxyRequest, tableName string, db dynamodbiface.DynamoDBAPI) (*events.APIGatewayProxyResponse, error) {
-	return nil
+	err := user.DeleteUser(req, tableName, db)
+	if err != nil {
+		return apiResponse(http.StatusBadRequest, ErrorBody{
+			aws.String(err.Error()),
+		})
+	}
+	return apiResponse(http.StatusOK, nil)
 }
 func UnhandleMethod() (*events.APIGatewayProxyResponse, error) {
 	return apiResponse(http.StatusMethodNotAllowed, ErrorMethodNotAllowed)

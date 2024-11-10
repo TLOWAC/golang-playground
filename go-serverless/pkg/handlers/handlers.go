04ee708ca5281ad53f50d7070dc29ba93405c256
TLOWAC <leechanghun55@gmail.com>
2024-11-10 21:54:05 +0900

    feat(handler): GetUser 기능 제작

--- go-serverless/pkg/handlers/handlers.go
+++ go-serverless/pkg/handlers/handlers.go
@@ -1,7 +1,45 @@
 package handlers
 
-func GetUser()        {}
-func CreateUser()     {}
-func PutUser()        {}
-func DeleteUser()     {}
-func UnhandleMethod() {}
+import (
+	"net/http"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
+)
+
+var ErrorMethodNotAllowed = "method not allowed"
+
+type ErrorBody struct {
+	ErrorMsg *string `json:"error,omitempty"`
+}
+
+func GetUser(req events.APIGatewayProxyRequest, tableName string, db dynamodbiface.DynamoDBAPI) (*events.APIGatewayProxyResponse, error) {
+
+	email := req.QueryStringParameters["email"]
+	if len(email) > 0 {
+		result, err := user.FetchUser(email, tableName, db)
+		if err != nil {
+			return apiResponse(http.StatusBadRequest, ErrorBody{aws.String(err.Error())})
+		}
+		return apiResponse(http.StatusOK, result)
+	}
+
+	result, err := user.FetchUsers(tableName, db)
+	if err != nil {
+		return apiResponse(http.StatusBadRequest, ErrorBody{aws.String(err.Error())})
+	}
+	return apiResponse(http.StatusOK, result)
+}
+func CreateUser(req events.APIGatewayProxyRequest, tableName string, db dynamodbiface.DynamoDBAPI) (*events.APIGatewayProxyResponse, error) {
+	return nil
+}
+func UpdateUser(req events.APIGatewayProxyRequest, tableName string, db dynamodbiface.DynamoDBAPI) (*events.APIGatewayProxyResponse, error) {
+	return nil
+}
+func DeleteUser(req events.APIGatewayProxyRequest, tableName string, db dynamodbiface.DynamoDBAPI) (*events.APIGatewayProxyResponse, error) {
+	return nil
+}
+func UnhandleMethod() (*events.APIGatewayProxyResponse, error) {
+	return apiResponse(http.StatusMethodNotAllowed, ErrorMethodNotAllowed)
+}

f6a0e2a57c203ebee03f1483b7f0ebf6d5ba779b
TLOWAC <leechanghun55@gmail.com>
2024-11-26 20:37:36 +0900

    feat: db connection 설정

--- go-mysql-jwt-test/cmd/main.go
+++ go-mysql-jwt-test/cmd/main.go
@@ -3,10 +3,27 @@ package main
 import (
 	"log"
 	"module/cmd/api"
+	"module/config"
+	"module/db"
+
+	"github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	server := api.NewAPISever(":8080", nil)
+	db, err := db.NewMySQLStorage(mysql.Config{
+		User:                 config.Env.DBUser,
+		Passwd:               config.Env.DBPassword,
+		Addr:                 config.Env.DBAddress,
+		DBName:               config.Env.DBName,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	server := api.NewAPISever(":8080", db)
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
 	}
